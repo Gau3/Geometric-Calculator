b3adef687997312ccf2222a9c35e438291f4ce02
Gau3 <87329744+Gau3@users.noreply.github.com>
2021-08-20 21:57:59 +0530

    Update Cylinder_Area_Volume.py

--- Cylinder_Area_Volume.py
+++ Cylinder_Area_Volume.py
@@ -3,13 +3,13 @@ height = float(input("Enter the height of cylinder: "))
 radian = float(input("Enter the radius of cylinder: "))
 def cylinder_area():
     sur_area = ((2*pi*radian) * height) + ((pi*radian**2)*2)
-    print("The area of rectangle is: ", sur_area)
+    print("The area of cylinder is: ", sur_area)
 cylinder_area()
 x = input("Enter V for calculating the Volume: ")
 if x == 'V':
     def cylinder_volume():
         volume = pi * radian * radian * height
-        print("The perimeter of rectangle is: ", volume)
+        print("The volume of cylinder is: ", volume)
     cylinder_volume()
 elif x != 'V':
     print("Please try with the correct input")
