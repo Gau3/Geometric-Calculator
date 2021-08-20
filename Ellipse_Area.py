e814672d77ed519698b0218589c50c5774b11c8f
Gau3 <87329744+Gau3@users.noreply.github.com>
2021-08-20 22:48:54 +0530

    Add files via upload

---
+++ Ellipse_Area.py
@@ -0,0 +1,7 @@
+pi = 22/7
+A = float(input("Enter the length of the semi-major axis of ellipse: "))
+B = float(input("Enter the lenght of the semi-minor axis of the ellipse:  "))
+def ellipse_area():
+    a = pi*A*B
+    print("The area of ellipse is: ", a)
+ellipse_area()
